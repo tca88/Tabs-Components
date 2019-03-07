4e11f4161cd6ac7a468bcdc97188af519d4c8b0d
tca88 <trishnaamin@gmail.com>
2019-03-07 16:13:09 -0500

    Create select() methods on both the TabLink and TabItem classes. Add a click event listener that calls the select method onClick.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,44 +13,50 @@ class TabLink {
     // console.log(this.itemElement);
     
     // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
+    this.tabItem = new TabItem(this.itemElement);
+    // console.log(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
-
+    this.link.addEventListener('click', () => this.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
+    // console.log(links);
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+
+    links.forEach(function(link){
+      link.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.link.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
 class TabItem {
-  constructor(element) {
+  constructor(item) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.item = item;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items = document.querySelectorAll('.tabs-item');
+    // console.log(items);
 
     // Remove the class "tabs-item-selected" from each element
-    
+    Array.from(items).forEach(function(item){
+      item.classList.remove('tabs-item-selected');
+    });
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
-  }
+    this.item.classList.add('tabs-item-selected');
 }
-
+}
 /* START HERE: 
 
 - Select all classes named ".tabs-link" and assign that value to the links variable
